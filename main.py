bbca1dd9061e52189519f6cb9e8cd5ece22eaf76
vadim cebanu <vadykdeejay@gmail.com>
2026-05-05 10:38:22 +0200

    Example created

---
+++ main.py
@@ -0,0 +1,22 @@
+notes = [
+    {"title": "Einkauf", "text": "Milch, Brot, Eier"},
+    {"title": "Arbeit", "text": "Backendcall um 11"},
+]
+
+def show_notes():
+    for note in notes:
+        print(f"Title: {note['title']}, Text: {note['text']}")
+
+def add_note():
+    pass 
+
+def delete_note():
+    pass 
+
+def update_note():
+    pass
+
+# add_note()
+# delete_note()
+# update_note()
+show_notes()
