530050dade3ca6105476c0c8f5f9cce25101c192
vadim cebanu <vadykdeejay@gmail.com>
2026-05-05 10:39:58 +0200

    function to add notice is created

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def show_notes():
         print(f"Title: {note['title']}, Text: {note['text']}")
 
 def add_note():
-    pass 
+    title = input("title for new note:")
+    text = input("text for new note:")
+    new_note = {"title" : title , "text" : text}
+    notes.append(new_note)
+    print(f"The note {new_note} is added!")
 
 def delete_note():
     pass 
